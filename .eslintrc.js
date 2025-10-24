9605fc921791135b1df6960ffc01e13d8c2b59f7
unknown <tieuvu4803@gmail.com>
2025-10-24 11:38:50 +0700

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@
     "env": {
         "browser": true,
         "node": true,
-        "es2021": true
+        "es2021": true,
+        "jest": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": 12
     },
     "rules": {
-        // Bạn có thể thêm các quy tắc tùy chỉnh ở đây sau
-        "no-unused-vars": "warn" // Cảnh báo nếu có biến không sử dụng
+        "no-unused-vars": "warn"
     }
 };
+
